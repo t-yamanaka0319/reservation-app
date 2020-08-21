4729e1b98f7fa528c78d1627c09fd7ea6b706c09
yamanaka <winc215yamanaka@gmail.com>
2020-08-21 17:39:39 +0900

    deploy settings

--- server/index.js
+++ server/index.js
@@ -3,19 +3,22 @@ const mongoose = require('mongoose');
 const config = require('./config/dev');
 const FakeDb = require('./fake-db');
 
-const productRoutes = require('./routes/products')
+const productRoutes = require('./routes/products');
+const path = require('path');
 
 mongoose.connect(config.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(
     () => {
-        const fakeDb = new FakeDb();
-        fakeDb.initDb();
+        if(process.env.NODE_ENV !== 'production') {
+            const fakeDb = new FakeDb();
+            //fakeDb.initDb();
+        }
     }
 );
 
-const app = express()
+const app = express();
 
 app.use('/api/v1/products', productRoutes);
 
@@ -23,7 +26,15 @@ app.use('/api/v1/products', productRoutes);
 //     res.json({'success': true})
 // });
 
-const PORT = process.env.PORT || '3001'
+if(process.env.NODE_ENV === 'production') {
+    const appPath = path.join( __dirname, '..', 'dist', 'reservation-app');
+    app.use(express.static(appPath));
+    app.get("*", function(req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || '3001';
 
 app.listen(PORT, function() {
     console.log('I am running!')
